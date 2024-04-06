d7637647fe39cb181a32f743f467e6b907a75944
attmhd <2022610013.attan@itp.ac.id>
2024-04-06 21:05:07 +0700

    Update fonts

--- UTS/tailwind.config.js
+++ UTS/tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
             colors: {
                 primary: "#f5f5f5",
                 second: "#3088D9",
+            },
+            fontFamily: {
+                body: ['Montserrat']
             }
         },
     },
