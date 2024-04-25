2d36d7f0cbfe64ae9f0e0d60f47199d8e3eba99f
attmhd <2022610013.attan@itp.ac.id>
2024-04-25 08:12:24 +0700

    Update UTS

---
+++ UTS/src/app.js
@@ -0,0 +1,17 @@
+// const navLink = document.querySelectorAll('.navlink')
+// const sectionId = document.querySelectorAll('.section')
+
+// let currentSect = 'about_me'
+// window.addEventListener('scroll', () => {
+//     sectionId.forEach(sectionId => {
+//         if (window.scrollY >= sectionId.offsetTop) {
+//             currentSect = sectionId.id
+//         }
+//     })
+
+//     navLink.forEach(navLink => {
+//         if (navLink.href.includes(currentSect)) {
+//             navLink.classList.add('aktif')
+//         }
+//     })
+// })
